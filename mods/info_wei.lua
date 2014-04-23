1686075069714076dfa679119c2297fbc39b9ce4
Kai <simaow@outlook.com>
2014-04-24 00:55:17 +1000

    delete some files

--- mods/info_wei.lua
+++
@@ -1,4 +0,0 @@
-wei_list = {}
-
-wei_list["±ùÌÇºùÂ«"] 	= {itemId="bingtang hulu", 	item="±ùÌÇºùÂ«", 	path="fly bj;s;", 	npcId="xiao fan", 	npc="Ğ¡··",	action=buyitem }
-wei_list["ÑÌ²İ"] 		= {itemId="yancao", 		item="ÑÌ²İ",		path="fly hmy;", 	npcId="lao zhe", 	npc="ÀÏÕß",	action=kill }
