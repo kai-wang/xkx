4b710ea21ef50751f3a8cb1bc6e85c7558a3663f
kai-wang <simaow@outlook.com>
2016-08-12 21:32:47 +1000

    remove gb skills

--- byj/byj.lua
+++ byj/byj.lua
@@ -26,11 +26,11 @@ auto_list = {"wei","shan","baobiao", "guanfu","guo","event","study","reconnect",
 weapon_list = {"juwei jian", "kunlun dao", "guangcheng jian", "zhaixing jian", "qiankun dao", "shenyuan jian"}
 
 pfm = {
-	[1] = {name="一剑化三清", 		desc="你大喝一声，剑招突变", 			cd=false},
-	[2] = {name="附骨缠身", 		desc="你大喝一声，缠身而上", 			cd=false},
+	--[1] = {name="一剑化三清", 		desc="你大喝一声，剑招突变", 			cd=false},
+	--[2] = {name="附骨缠身", 		desc="你大喝一声，缠身而上", 			cd=false},
 	[3] = {name="璀璨七星", 		desc="你倒踩七星步法", 					cd=false},
 	[4] = {name="双手互博之术", 	desc="你平心静气", 						cd=false},
-	[5] = {name="定阳针", 			desc="你脚下左弓右箭", 					cd=false},
+	--[5] = {name="定阳针", 			desc="你脚下左弓右箭", 					cd=false},
 	[6] = {name="一指乾坤", 		desc="你使出一阳指绝技", 				cd=false},
 	[7] = {name="阳关三叠", 		desc="你凝气于指，一式", 				cd=false},
 	[8] = {name="天羽狂舞",			desc="你使出天羽奇剑中的绝招",			cd=false},
@@ -40,30 +40,32 @@ pfm = {
 	[10]= {name="凌波微步",			desc="你提起真气，依照先天伏羲六十四卦",cd=false},
 	[11]= {name="长相思",			desc="你使出痴心情长剑之绝舞",				cd=false},
 	[12]= {name="月光如影",			desc="你照着月光的映射",				cd=false},
-	[13]= {name="六阳融雪",			desc="你使出天山六阳掌之绝技『六阳融雪』",			cd=false},
-	[14]= {name="熊熊圣火",			desc="你盘膝而坐，双手十指张开",		cd=false},
-	[15]= {name="如影如幻",			desc="你长空而起，化作一只青蝠",		cd=false},
-	[16]= {name="金蛇万道",			desc="你大喝一声，猛攻数招", 			cd=false},
-	[17]= {name="吸焰", 			desc="你使出圣火令法「吸焰」令", 		cd=false},
-	[18]= {name="牧野鹰扬",			desc="你使出搜骨鹰爪功绝技", 			cd=false},
+	--[13]= {name="六阳融雪",			desc="你使出天山六阳掌之绝技『六阳融雪』",			cd=false},
+	--[14]= {name="熊熊圣火",			desc="你盘膝而坐，双手十指张开",		cd=false},
+	--[15]= {name="如影如幻",			desc="你长空而起，化作一只青蝠",		cd=false},
+	--[16]= {name="金蛇万道",			desc="你大喝一声，猛攻数招", 			cd=false},
+	--[17]= {name="吸焰", 			desc="你使出圣火令法「吸焰」令", 		cd=false},
+	--[18]= {name="牧野鹰扬",			desc="你使出搜骨鹰爪功绝技", 			cd=false},
 	[19]= {name="天山月影",			desc="你使出月影舞步的绝技「天山月影」",				cd=false},
-	[20]= {name="透骨针",			desc="你悄悄运起透骨针",				cd=false},
-	[21]= {name="精失诀",			desc="你凝神定气，使出七伤拳总诀中的「精失诀」", cd=false},
+	--[20]= {name="透骨针",			desc="你悄悄运起透骨针",				cd=false},
+	--[21]= {name="精失诀",			desc="你凝神定气，使出七伤拳总诀中的「精失诀」", cd=false},
 	[22]= {name="七拳七伤",			desc="你仰天怒吼，激起全身力量使出七伤拳终极绝技", cd=false},
-	[23]= {name="意恍惚诀",			desc="你凝神定气，使出七伤拳总诀中的「意恍惚诀」", cd=false},
-	[24]= {name="连环夺命诀",		desc="你使出苗家剑法之「连环夺命诀」", 			cd=false},
-	[25]= {name="绝命刺", 			desc="你突然运足内力，只听手中的", cd=false},
-	[26]= {name="三焦齐逆诀",		desc="你凝神定气，企图使出七伤拳总诀中的「三焦齐逆诀」", cd=false},
-	[27]= {name="伤肺诀",			desc="你凝神定气，使出七伤拳总诀中的「伤肺诀」", 	cd=false},
-	[28]= {name="损心诀",			desc="你凝神定气，使出七伤拳总诀中的「损心诀」",	cd=false},
-	[29]= {name="藏离诀",			desc="你凝神定气，使出七伤拳总诀中的「藏离诀」",	cd=false},
-	[30]= {name="摧肝肠诀",			desc="你凝神定气，使出七伤拳总诀中的「摧肝肠诀」",	cd=false},
+	--[23]= {name="意恍惚诀",			desc="你凝神定气，使出七伤拳总诀中的「意恍惚诀」", cd=false},
+	--[24]= {name="连环夺命诀",		desc="你使出苗家剑法之「连环夺命诀」", 			cd=false},
+	--[25]= {name="绝命刺", 			desc="你突然运足内力，只听手中的", cd=false},
+	--[26]= {name="三焦齐逆诀",		desc="你凝神定气，企图使出七伤拳总诀中的「三焦齐逆诀」", cd=false},
+	--[27]= {name="伤肺诀",			desc="你凝神定气，使出七伤拳总诀中的「伤肺诀」", 	cd=false},
+	--[28]= {name="损心诀",			desc="你凝神定气，使出七伤拳总诀中的「损心诀」",	cd=false},
+	--[29]= {name="藏离诀",			desc="你凝神定气，使出七伤拳总诀中的「藏离诀」",	cd=false},
+	--[30]= {name="摧肝肠诀",			desc="你凝神定气，使出七伤拳总诀中的「摧肝肠诀」",	cd=false},
 	[31]= {name="雷霆一击",			desc="你默运混天气功，施展出「雷霆一击」", 			cd=false},
 	[32]= {name="亢龙三悔",			desc="你深吸一口气，将浑天真气提于胸前，大喝一声说道",cd=false},
 	[33]= {name="沧海一笑",			desc="你强提真气，身法突变得迷离，有如纵横之式",	cd=false},
 	[34]= {name="幻影",			desc="你前后游走，身形闪动，手中钢刀越使越快，幻起无数刀光化出数个身形", cd=false},
 	[35]= {name="刀刀相连", 		desc="在一片刀光中，一刀劈了过来", cd=false},
-	[36]= {name="无色无相", desc="你使出小无相绝技「无色无相」，招式变幻莫测", cd=false}
+	[36]= {name="无色无相", desc="你使出小无相绝技「无色无相」，招式变幻莫测", cd=false},
+	[37]= {name="天马行空", desc="你使出身空行，身形回转，如天马跃空而行", cd=false},
+	[38]= {name="神功震敌", desc="你默运神功，使出猴拳「震」字诀", cd=false}
 }
 
 function set_cd_status(l, flag, color)
@@ -106,9 +108,9 @@ busy_perform_array = {
 			end
 	},
 
-	[4] = { i = 12,
+	[4] = { i = 37,
 			action = function()
-				Execute("enable move xiaoyaoyou;perform move.canghaiyixiao " .. var.pfm_target)
+				Execute("enable move shenkong-xing;perform move.tianmaxingkong " .. var.pfm_target)
 			end
 	}
 }
@@ -133,7 +135,7 @@ attack_perform_array = {
 
 	[3] = { i = 22,
 			action = function()
-				Execute("unwield all;bei none;bei qishang-quan;perform cuff.qishang " .. var.pfm_target)
+				Execute("unwield all;enable cuff qishang-quan;bei none;bei qishang-quan;perform cuff.qishang " .. var.pfm_target)
 			end
 	},
 
@@ -210,6 +212,12 @@ attack_perform_array = {
 				local wp = choose_blade()
 				Execute("enable blade xue-dao;wield " .. wp .. ";jiali max;enable move yueying-wubu;perform move.yueying " .. var.pfm_target .. ";jiali 0")
 			end
+	},
+
+	[16] = { i = 38,
+			action = function()
+				Execute("enable cuff houquan;unwield all;perform cuff.zhen")
+			end
 	}
 }
 
@@ -224,10 +232,11 @@ ttask_attack_list = { 1, 2, 3, 4 }
 
 busy_list = { 1, 2, 3, 4 }
 attack_list1 = { 7, 3, 6 }
-attack_list2 = { 3, 8, 9, 5, 6 }
-attack_list3 = { 7, 5, 13}
+--attack_list2 = { 3, 8, 9, 5, 6 }
+attack_list2 = { 3, 8, 14, 6, 7 }
+attack_list3 = { 7, 16, 13}
 attack_list4 = { 6 }
-attack_list5 = { 3, 14, 6, 7, 5 }
+attack_list5 = { 3, 14, 6, 7, 16 }
 
 study_list = {
 	--{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu finger 10000;et;set study done", post_action="fly wm;e;s;s;s;w;w;u;gamble big skill finger 2000"}
@@ -245,7 +254,7 @@ function choose_sword()
 end
 
 function powerup()
-	Execute("enable force huntian-qigong;yun powerup;enable force beiming-shengong;yun shield;yun beiming")
+	Execute("enable force xiaowuxiang;yun powerup;enable force beiming-shengong;yun shield;yun beiming")
 end
 
 function buff(menpai)
@@ -254,13 +263,13 @@ function buff(menpai)
 		local r1,r2,r3 = re:match(menpai)
 		if(r3 ~= nil) then
 			print("换金系内功了.......")
-			Execute("unwield all;enable force huntian-qigong;yun shengang;perform strike.xianglong;enable force xiaowuxiang;")
+			Execute("enable force xiaowuxiang;")
 			return
 		end
 	end
 
 	print("换水系内功了.......")
-	Execute("unwield all;enable force huntian-qigong;yun shengang;enable force beiming-shengong;yun beiming;perform strike.xianglong")
+	Execute("enable force beiming-shengong;yun beiming")
 end
 
 
