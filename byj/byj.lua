e13aeaf1065a6ec2ed8bb2e8bcab0c7da7ec39c4
kai-wang <simaow@outlook.com>
2016-09-15 21:29:49 +1000

    change research

--- byj/byj.lua
+++ byj/byj.lua
@@ -281,7 +281,7 @@ attack_list5 = { 3, 12, 17, 6, 7 }	-- xiao
 
 study_list = {
 	--{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu finger 10000;et;set study done", post_action="fly wm;e;s;s;s;w;w;u;gamble big skill finger 2000"}
-	{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu beiming-shengong 1000", wear_int = true, research = true }
+	{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu xiaowuxiang 1000", wear_int = true, research = true }
 		--{ loc = "fly mj", cmd = "xue wei strike 100;et",pre_action="bai wei yixiao", post_action="bai xie xun"}
 }
 
