5ebaf52abd36c0e5801dc2895c59971ec1d80fb2
kai-wang <simaow@outlook.com>
2016-09-23 23:06:22 +1000

    change equip

--- byj/byj.lua
+++ byj/byj.lua
@@ -284,7 +284,7 @@ attack_list5 = { 3, 12, 17, 6, 7 }	-- xiao
 
 study_list = {
 	--{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu finger 10000;et;set study done", post_action="fly wm;e;s;s;s;w;w;u;gamble big skill finger 2000"}
-	{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu xiaowuxiang 1000", wear_int = true, research = true }
+	{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu wuzheng-xinfa 1000", wear_int = true, research = true }
 		--{ loc = "fly mj", cmd = "xue wei strike 100;et",pre_action="bai wei yixiao", post_action="bai xie xun"}
 }
 
@@ -389,7 +389,7 @@ end
 
 function fight_wear(f_done)
 	wait.make(function()
-		Execute("tuo all;wear kunlun xue;wear xiaocui xue;wear shenshui mao;wear lianyu yi;wear tianlong yi;wear lianyu hushou;wear yecha ring;give byj kunlun qin")
+		Execute("tuo all;wear kunlun xue;wear xiaocui xue;wear shenshui mao;wear haoqi yi;wear tianlong yi;wear lianyu hushou;wear yecha ring;give byj kunlun qin")
 		wait.time(1)
 		core.busytest(function() call(f_done) end)
 	end)
