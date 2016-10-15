f92d1bd051472c01469a21c60f74ffef22441105
kai-wang <simaow@outlook.com>
2016-10-16 06:15:57 +1100

    fix priority of sstask

--- mods/auto.lua
+++ mods/auto.lua
@@ -218,7 +218,7 @@ local tasks = {
 		
 		priority = function()
 			-- exceed the limit
-			if(var.double_bonus == ¡°true¡±) then return 13 else return 15 end
+			if(var.double_bonus == "true") then return 13 else return 15 end
 			--return 15
 		end
 	},
