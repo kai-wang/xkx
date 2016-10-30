bb1afe53e614d8db93809dee1abbf00f7ae05f33
kai-wang <simaow@outlook.com>
2016-10-30 23:18:56 +1100

    check ding

--- mods/misc.lua
+++ mods/misc.lua
@@ -40,7 +40,7 @@ end
 
 function check_ding(f_ok, f_fail)
 	wait.make(function()
-		Execute("fly xx;look ding")
+		Execute("fly wm;nw;look ding")
 		local l, w = wait.regexp("^(> )*(你要看什么)|(木鼎)|(神木王鼎).*$", 5)
 		if(not l or l:match("你要看什么")) then 
 			print("没有鼎")
