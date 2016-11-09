199e7f8e895336cfc70013fe37e219384dab32de
kai-wang <simaow@outlook.com>
2016-11-09 21:29:23 +1100

    add crab

--- crab/crab.lua
+++ crab/crab.lua
@@ -19,22 +19,26 @@ var.dig_dummy = "lbt"
 var.chatter_whitelist = "byj;lbt;happy;"
 var.chatter_blacklist = ""
 var.weapon = "qin"
-var.weapon2 = "dao"
+var.weapon2 = "qin"
+var.kantou_wp = "dao;wield qin"
 var.study_seq = 1
 var.fast_mode = 1
 var.xiao_full = 0
+var.xiao_retry_threshold = 3
 var.guo_kill_place = "fly mj"
-var.task_walk_danger_level = 4
+var.task_walk_danger_level = 5
 var.gf_walk_danger_level = 5
 var.ss_walk_danger_level = 5
-var.xiao_walk_danger_level = 4
-var.ss_shuffle = 1
+var.xiao_walk_danger_level = 5
+var.ss_shuffle = 0
+var.ss_can_stop = 1
 var.study_threshold = 10000
 var.kantou_flag = true
+var.sleep_loc = "fly bt;n;w;n" 
 
-auto_list = {"wei", "guanfu","guo","event","reconnect", "sstask", "xiao", "double", "baobiao", "shan", "wine", "task"}
+auto_list = {"wei", "guanfu","guo","event","reconnect", "xiao", "double", "baobiao", "shan", "wine", "task", "wait_for_task"}
 
-weapon_list = {"kunlun qin", "kunlun dao", "haoqi qin", "tiandi dao", "qiankun dao", "lianyu qin"}
+weapon_list = {"tianqu qin", "tianyu qin", "guiqi qin", "tianfeng qin"}
 
 pfm = {
 	[1] = {name="一剑化三清", 		desc="你大喝一声，剑招突变", 			cd=false},
@@ -76,7 +80,7 @@ pfm = {
 	[35]= {name="刀刀相连", 	desc="在一片刀光中，一刀劈了过来", cd=false},
 	[36]= {name="无色无相", 	desc="你使出小无相绝技「无色无相」，招式变幻莫测", cd=false},
 	[37]= {name="天马行空", 	desc="你使出身空行，身形回转，如天马跃空而行", cd=false},
-	[38]= {name="灵犀一指", 	desc="你双手一前一后，缓缓伸出", cd=false}
+	[38]= {name="亡魂一击", 	desc="你突然将双掌变爪合于胸前，然后象风车般急转数圈", cd=false}
 }
 
 function set_cd_status(l, flag, color)
@@ -106,22 +110,22 @@ busy_perform_array = {
 			end
 	},
 
-	[2] = { i = 34,
-			action = function()
-				local wp = choose_blade()
-				Execute("unwield all;enable blade hujia-daofa;wield " .. wp .. ";perform blade.huanying " .. var.pfm_target .. ";unwield all")
-			end
-	},
-
-	[3] = { i = 12,
+	[2] = { i = 12,
 			action = function()
 				Execute("enable move yueying-wubu;perform move.yueguangruying " .. var.pfm_target)
 			end
 	},
 
-	[4] = { i = 37,
+	[3] = { i = 9,
 			action = function()
-				Execute("enable move shenkong-xing;perform move.tianmaxingkong " .. var.pfm_target)
+				local wp = choose_sword()
+				Execute("enable parry tianyu-qijian;unwield all;wield " .. wp .. ";perform parry.san " .. var.pfm_target .. ";unwield all")
+			end
+	},
+
+	[4] = { i = 38,
+			action = function()
+				Execute("unwield all;enable claw jiuyin-baiguzhao;perform claw.zhua " .. var.pfm_target)
 			end
 	}
 }
@@ -130,118 +134,40 @@ busy_perform_array = {
 -- attack perform
 --
 attack_perform_array = {
-	[2] = { i = 36,
-			action = function()
-				local wp = choose_xuedao()
-				choose_force()
-				Execute("unwield all;wield " .. wp .. ";enable blade xue-dao;enable force xiaowuxiang;jiali max;yun wuxiang " .. var.pfm_target .. ";jiali 0;")
-			end
-	},
-
-	[1] = { i = 35,
-			action = function()
-				local wp = choose_xuedao()
-				choose_force()
-				Execute("unwield all;enable blade xue-dao;enable parry hujia-daofa;wield " .. wp .. ";jiali max;perform parry.lian " .. var.pfm_target .. ";jiali 0;unwield all")
-			end
-	},
-
-	[3] = { i = 22,
+	[1] = { i = 22,
 			action = function()
 				choose_force()
 				Execute("unwield all;enable cuff qishang-quan;bei none;bei qishang-quan;perform cuff.qishang " .. var.pfm_target)
 			end
 	},
 
-	[4] = { i = 4,
+	[2] = { i = 4,
 			action = function()
 				choose_force()
-				Execute("bei none;enable finger sun-finger;bei sun-finger;unwield all;hubo " .. var.pfm_target)
+				Execute("bei none;enable claw jiuyin-baiguzhao;bei jiuyin-baiguzhao;unwield all;hubo " .. var.pfm_target)
 			end
 	},
 
-	[5] = { i = 20,
-			action = function()
-				local wp = choose_sword()
-				Execute("unwield all;wield " .. wp .. ";enable sword yuxiao-jian;perform sword.tianwai " .. var.pfm_target)
-			end
-	},
-
-	[6] = { i = 4,
+	[3] = { i = 4,
 			action = function()
 				choose_force()
 				Execute("unwield all;hubo " .. var.pfm_target)
 			end
 	},
 
-	[7] = { i = 35,
+	[4] = { i = 36,
 			action = function()
-				local wp = choose_blade()
-				choose_force()
-				Execute("unwield all;wield " .. wp .. ";enable blade hujia-daofa;perform blade.lian " .. var.pfm_target)
+				Execute("unwield all;enable force xiaowuxiang;bei none;enable claw yingzhua-gong;enable hand fengyun-shou;bei fengyun-shou;bei yingzhua-gong;enable claw jiuyin-baiguzhao;enable hand zhemei-shou;yun wuxiang " .. var.pfm_target)
 			end
 	},
 
-
-	[8] = { i = 36,
+	[5] = { i = 19,
 			action = function()
-				choose_force()
-				Execute("unwield all;enable force xiaowuxiang;bei none;enable finger sun-finger;bei sun-finger;yun wuxiang " .. var.pfm_target)
+				Execute("enable move yueying-wubu;bei none;bei jiuyin-baiguzhao;perform move.yueying")
 			end
 	},
 
-	[9] = { i = 24,
-			action = function()
-				Execute("unwield all;bei none;bei huoyan-dao;perform strike.xiuluo " .. var.pfm_target)
-			end
-	},
-
-	[10] = { i = 32,
-			action = function()
-				Execute("unwield all;enable force huntian-qigong;perform strike.sanhui " .. var.pfm_target .. ";wield dao")
-			end
-	},
-
-	[11] = { i = 36,
-			action = function()
-				Execute("unwield all;enable force xiaowuxiang;yun wuxiang " .. var.pfm_target .. ";wield dao")
-			end
-	},
-
-	[12] = { i = 36,
-			action = function()
-				Execute("unwield all;enable force xiaowuxiang;bei none;enable finger tanzhi-shentong;bei lanhua-shou;bei tanzhi-shentong;enable finger sun-finger;yun wuxiang " .. var.pfm_target)
-			end
-	},
-
-	[13] = { i = 19,
-			action = function()
-				Execute("enable move yueying-wubu;bei none;bei sun-finger;perform move.yueying")
-			end
-	},
-
-	[14] = { i = 11,
-			action = function()
-				local wp = choose_sword()
-				choose_force()
-				Execute("unwield all;wield " .. wp .. ";enable sword chixin-qingchang-jian;perform sword.xiangsi")
-			end
-	},
-
-	[15] = { i = 35,
-			action = function()
-				local wp = choose_blade()
-				Execute("enable blade xue-dao;wield " .. wp .. ";jiali max;enable move yueying-wubu;perform move.yueying " .. var.pfm_target .. ";jiali 0")
-			end
-	},
-
-	[16] = { i = 38,
-			action = function()
-				Execute("enable finger taizhi-shentong;unwield all;perform finger.lingxi")
-			end
-	},
-
-	[17] = { i = 14,
+	[6] = { i = 14,
 			action = function()
 				local wp = choose_sword()
 				choose_force()
@@ -250,7 +176,7 @@ attack_perform_array = {
 			end
 	},
 
-	[18] = { i = 8,
+	[7] = { i = 8,
 			action = function()
 				local wp = choose_sword()
 				choose_force()
@@ -259,7 +185,7 @@ attack_perform_array = {
 			end
 	},
 
-	[19] = { i = 9,
+	[8] = { i = 9,
 			action = function()
 				local wp = choose_sword()
 				choose_force()
@@ -269,25 +195,25 @@ attack_perform_array = {
 }
 
 task_busy_list = { 1, 2, 3, 4 }
-task_attack_list = { 1, 2, 17, 4, 3, 18 }
+task_attack_list = { 6, 4, 7, 1, 3 }
 
 gf_busy_list = { 1, 3 }
-gf_attack_list = { 6, 7 }
+gf_attack_list = { 3, 1 }
 
 ttask_busy_list = { 1, 2, 3, 4 }
 ttask_attack_list = { 1, 2, 3, 4 }
 
 busy_list = { 1, 2, 3, 4 }
-busy_list2 = { 1, 3, 4, 2 }
-attack_list1 = { 7, 3, 6 } 			-- shan / blocker
-attack_list2 = { 3, 8, 17, 4, 7 }	-- xiao
-attack_list3 = { 7, 19, 13}			-- wei / xiao
-attack_list4 = { 6 }				-- shan / wei
-attack_list5 = { 3, 12, 17, 6, 7 }	-- xiao
+busy_list2 = { 1, 2, 3, 4 }
+attack_list1 = { 3, 1, 7 } 			-- shan / blocker
+attack_list2 = { 6, 4, 7, 1, 3 }	-- xiao
+attack_list3 = { 3, 5, 8}			-- wei / xiao
+attack_list4 = { 3 }				-- shan / wei
+attack_list5 = { 6, 4, 7, 1, 3 }	-- xiao
 
 study_list = {
 	--{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu finger 10000;et;set study done", post_action="fly wm;e;s;s;s;w;w;u;gamble big skill finger 2000"}
-	{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu dodge 10000", wear_int = true, interval = 0.8, post_action="fly wm;e;s;s;s;w;w;u;gamble big skill dodge 2000"}
+	{ loc = "fly wm;e;n;e;e;n;n;", cmd = "yanjiu wuzheng-xinfa 5000", wear_int = true, interval = 0.8}
 	--{ loc = "fly mj", cmd = "xue wei strike 100;et",pre_action="bai wei yixiao", post_action="bai xie xun"}
 }
 
@@ -347,7 +273,7 @@ function buff(menpai)
 		r1,r2,r3 = re:match(menpai)
 		if(r3 ~= nil) then
 			print("换土系内功了.......")
-			var.choose_force = "xixing-dafa"
+			var.choose_force = "beiming-shengong"
 			Execute("perform dodge.mengyulingbo")
 			return
 		end
@@ -397,7 +323,7 @@ end
 function int_wear(f_done)
 	wait.make(function()
 		Execute("unwield all;tuo all")
-		Execute("wield qiankun dao;wear haoqi yi;wear shentong mao;wear shenshui mao;wear xunzhang;wear yaohou xue;wear jiulong hushou;wear sanqing ring")
+		Execute("wield tianyu qin;wield guiqi qin;wear xunzhang;wear ring")
 		wait.time(1)
 		call(f_done)
 	end)
@@ -405,7 +331,7 @@ end
 
 function fight_wear(f_done)
 	wait.make(function()
-		Execute("tuo all;wear shentong xue;wear bagua xue;wear shentong mao;wear shenshui mao;wear taiji yi;wear xuanyuan yi;wear lianyu hushou;wear yecha ring;give byj qiankun dao")
+		Execute("tuo all;wear tianfeng qin;wear ningshuang xue;wear luyu mao;wear taimen yi;wear yanlong hushou;wear wuxing jiezhi")
 		wait.time(1)
 		core.busytest(function() call(f_done) end)
 	end)
